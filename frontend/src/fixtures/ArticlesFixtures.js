abc7b6e5d8b634b3f25e42632cc26b59126da819
Justin Hao <justinhao@ucsb.edu>
2024-02-15 18:14:05 -0800

    JH- added articles fictures

---
+++ frontend/src/fixtures/ArticlesFixtures.js
@@ -0,0 +1,39 @@
+const ArticlesFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "test",
+        "url": "test.com",
+        "explanation": "hello world",
+        "email": "test@ucsb.edu",
+        "dateAdded": "2022-01-02T12:00:00"
+    },
+    threeArticles: [
+        {
+            "id": 1,
+            "title": "test",
+            "url": "test.com",
+            "explanation": "hello world",
+            "email": "test@ucsb.edu",
+            "dateAdded": "2022-01-02T12:00:00"
+        },
+        {
+            "id": 2,
+            "title": "test2",
+            "url": "test2.com",
+            "explanation": "hello world2",
+            "email": "tes2t@ucsb.edu",
+            "dateAdded": "2022-01-02T12:00:00"
+        },
+        {
+            "id": 3,
+            "title": "test3",
+            "url": "test3.com",
+            "explanation": "hello world3",
+            "email": "test3@ucsb.edu",
+            "dateAdded": "2022-01-02T12:00:00"
+        }
+    ]
+};
+
+
+export { articlesFixtures };
