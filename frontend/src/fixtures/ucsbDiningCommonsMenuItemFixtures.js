1da1bff8781a92d291612ad833ddb33827a580dc
El-Beetor <vhg@ucsb.edu>
2024-02-16 12:52:19 -0800

    vhg-added fixture

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneUCSBDiningCommonsMenuItem: {
+        "id": 1,
+        "diningCommonsCode ": "1234",
+        "name": "Pizza",
+        "statioin": "Deli"
+    },
+    threeUCSBDiningCommonsMenuItems: [
+        {
+            "id": 1,
+            "diningCommonsCode ": "1234",
+            "name": "Gyro",
+            "statioin": "Euro"
+        },
+        {
+            "id": 2,
+            "diningCommonsCode ": "4321",
+            "name": "Ceaser Salad",
+            "statioin": "Greenery"
+        },
+        {
+            "id": 3,
+            "diningCommonsCode ": "5555",
+            "name": "Burger",
+            "statioin": "American"
+        }
+    ]
+};
+
+
+export { ucsbDiningCommonsMenuItemFixtures };
