4a85bfe66342cc4665497422ad8062196deb89fa
El-Beetor <vhg@ucsb.edu>
2024-02-18 21:19:17 -0800

    vhg-added table for menu items

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,26 +1,26 @@
 const ucsbDiningCommonsMenuItemFixtures = {
     oneUCSBDiningCommonsMenuItem: {
         "id": 1,
-        "diningCommonsCode ": "Carrillo",
+        "diningCommonsCode": "Carrillo",
         "name": "Pizza",
         "statioin": "Deli"
     },
     threeUCSBDiningCommonsMenuItems: [
         {
             "id": 1,
-            "diningCommonsCode ": "Ortega",
+            "diningCommonsCode": "Ortega",
             "name": "Gyro",
             "statioin": "Euro"
         },
         {
             "id": 2,
-            "diningCommonsCode ": "DLG",
+            "diningCommonsCode": "DLG",
             "name": "Ceaser Salad",
             "statioin": "Greenery"
         },
         {
             "id": 3,
-            "diningCommonsCode ": "Portola",
+            "diningCommonsCode": "Portola",
             "name": "Burger",
             "statioin": "American"
         }
---
+++ frontend/src/stories/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemTable.stories.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import UCSBDiningCommonsMenuItemTable from 'main/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemTable';
+import { UCSBDiningCommonsMenuItemFixtures } from 'fixtures/ucsbDiningCommonsMenuItemFixtures';
+import { currentUserFixtures } from 'fixtures/currentUserFixtures';
+import { rest } from "msw";
+
+export default {
+    title: 'components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemTable',
+    component: UCSBDiningCommonsMenuItemTable
+};
+
+const Template = (args) => {
+    return (
+        <UCSBDiningCommonsMenuItemTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    UCSBDiningCommonsMenuItems: []
+};
+
+export const ThreeItemsOrdinaryUser = Template.bind({});
+
+ThreeItemsOrdinaryUser.args = {
+    UCSBDiningCommonsMenuItems: UCSBDiningCommonsMenuItemFixtures.threeUCSBDiningCommonsMenuItems,
+    currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsAdminUser = Template.bind({});
+ThreeItemsAdminUser.args = {
+    UCSBDiningCommonsMenuItems: UCSBDiningCommonsMenuItemFixtures.threeUCSBDiningCommonsMenuItems,
+    currentUser: currentUserFixtures.adminUser,
+}
+
+ThreeItemsAdminUser.parameters = {
+    msw: [
+        rest.delete('/api/UCSBDiningCommonsMenuItem', (req, res, ctx) => {
+            window.alert("DELETE: " + JSON.stringify(req.url));
+            return res(ctx.status(200),ctx.json({}));
+        }),
+    ]
+};
