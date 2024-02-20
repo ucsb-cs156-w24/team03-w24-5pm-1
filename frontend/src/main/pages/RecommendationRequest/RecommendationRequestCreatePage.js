ebac215b15067f2d239cbed0d5457b88e34dfedf
ChrisWoolson <cw.woolson@gmail.com>
2024-02-20 13:24:04 -0800

    CW-Implemented CreatePage Recommendation Reques and testst

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.js
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.js
@@ -1,12 +1,47 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RecommendationRequestForm from "main/components/RecommendationRequests/RecommendationRequestForm";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function RecommendationRequestCreatePage() {
+export default function RecommendationRequestCreatePage({storybook=false}) {
+  const objectToAxiosParams = (recommendationRequest) => ({
+    url: "/api/RecommendationRequest/post",
+    method: "POST",
+    params: {
+     requesterEmail: recommendationRequest.requesterEmail,
+     professorEmail: recommendationRequest.professorEmail,
+     explanation: recommendationRequest.explanation,
+     dateRequested: recommendationRequest.dateRequested,
+     dateNeeded: recommendationRequest.dateNeeded,
+     done: recommendationRequest.done
+    }
+  });
 
-  // Stryker disable all : placeholder for future implementation
+  const onSuccess = (recommendationRequest) => {
+    toast(`New RecommendationRequest Created - id: ${recommendationRequest.id}, explanation: ${recommendationRequest.explanation}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+     { onSuccess },
+     ["/api/RecommendationRequest/all"]
+  );
+  
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+  if (isSuccess && !storybook) {
+    return <Navigate to="/RecommendationRequest" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New Recommendation Request</h1>
+        <RecommendationRequestForm submitAction={onSubmit}/>
       </div>
     </BasicLayout>
   )
